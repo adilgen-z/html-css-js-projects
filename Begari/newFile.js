f7b866a5f1527cdb7058da18b06d5e73659463c9
Adil <150450888+adilgen-z@users.noreply.github.com>
2024-07-09 11:26:15 +0530

    Delete Begari directory

--- Begari/newFile.js
+++
@@ -1,8 +0,0 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    let btn = document.querySelector('.btn');
-    let body = document.querySelector('body');
-    btn.oneclick = function () {
-        body.classList.toggle('on');
-    };
-}
